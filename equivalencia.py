1a4441e49774cc085ea1595448be42aad0539375
Lucas <l.arribada123@gmail.com>
2026-08-17 20:03:44 -0300

    Implementação do cadastro e consulta no dicionário

--- equivalencia.py
+++ equivalencia.py
@@ -1,3 +1,10 @@
 class EquivalenciasCurriculares:
+
     def __init__(self):
         self._mapa = {}
+        
+    def cadastrar(self, codigo_antigo, codigo_novo):
+        self._mapa[codigo_antigo] = codigo_novo
+
+    def consultar(self, codigo_antigo):
+        return self._mapa.get(codigo_antigo)
