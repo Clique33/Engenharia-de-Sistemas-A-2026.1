225963d9c8d3c7de514b9eb9979b6ccd8c774837
Your Name <you@example.com>
2026-08-18 14:12:09 -0300

    edicoes

--- curriculo.py
+++ curriculo.py
@@ -7,24 +7,39 @@ class Disciplina:
 
 class Curriculo:
     
-    def __init__(self, curso, disciplinas=[None], periodos=8):
+    def __init__(self, curso, disciplinas=[], periodos=8):
 
         self.curso = curso
         self.disciplinas = disciplinas
         self.periodos = periodos
 
-    def adicionar_disciplina(self, disciplina):
+    def adicionar_disciplina(self, disciplina: Disciplina):
 
-        self.disciplinas.append(disciplina)
+        buffer = disciplina
 
+        self.disciplinas.append(buffer)
+
+
+
+        
     def mostrar_disciplinas(self):
 
-        vazio = 0
-        print(type(self.disciplinas))        
 
-        # for d in self.disciplinas:
-        #     print(d.nome)
+        tamanho = len(self.disciplinas)
+
+        if tamanho == 0: 
+            print('Curso sem disciplinas')
+            return -1
+
+        for disciplina in self.disciplinas:
+            print(disciplina.nome)
 
 
-c = Curriculo()
-c.adicionar_disciplina()
+calculo = Disciplina("calculo", 1)
+
+
+c = Curriculo("Engenharia da computacao")
+print(c.curso)
+c.adicionar_disciplina(calculo)
+
+c.mostrar_disciplinas()
