777116057ed1b163ff9ae4f86ebf182c3d0b197d
rafael <rafael100brasil123@gmail.com>
2026-08-24 12:30:25 -0300

    trabalho issue02

--- curriculo.py
+++ curriculo.py
@@ -1,8 +1,9 @@
 
 class Disciplina:
-    def __init__(self, nome, periodo):
+    def __init__(self, nome, codigo, periodo):
         self.nome = nome 
         self.periodo = periodo
+        self.codigo = codigo
 
 
 class Curriculo:
@@ -17,10 +18,32 @@ class Curriculo:
 
         buffer = disciplina
 
+        for d in self.disciplinas:
+            if disciplina.codigo == d.codigo:
+                print("a disciplina ja existe")
+                return -1
+
         self.disciplinas.append(buffer)
 
 
+    def buscar_disciplina(self, codigo):
 
+        for disciplina in self.disciplinas:
+            if (codigo == disciplina.codigo):
+                return disciplina
+
+        return -1
+
+    def disciplinas_por_periodo(self, periodo):
+
+        print(f"Disciplinas do {periodo}º: ")
+        for disciplina in self.disciplinas:
+            if periodo == disciplina.periodo:
+
+                print(f"Codigo da disciplina: {disciplina.codigo}")
+                print(f"Nome da disciplina: {disciplina.nome}")
+                print(f"Periodo da disciplina: {disciplina.periodo} \n")
+                
         
     def mostrar_disciplinas(self):
 
@@ -28,18 +51,48 @@ class Curriculo:
         tamanho = len(self.disciplinas)
 
         if tamanho == 0: 
-            print('Curso sem disciplinas')
+            print("Curso sem disciplinas \n")
+
             return -1
+        
+        print(f"\nImprimindo disciplinas do curso de {self.curso}:")
 
         for disciplina in self.disciplinas:
-            print(disciplina.nome)
+            print(f"\nCodigo da disciplina: {disciplina.codigo}")
+            print(f"Nome da disciplina: {disciplina.nome}")
+            print(f"Periodo da disciplina: {disciplina.periodo} \n")
+            
+
+#TESTE
+
+def criaCurriculo(nome="curso", disciplinas=[]):
+    
+    
+    c = Curriculo(nome)
+    
+    for disciplina in disciplinas:
+        d = Disciplina(disciplina.nome, disciplina.codigo, disciplina.periodo)
+        c.adicionar_disciplina(d)
+
+    c.mostrar_disciplinas()
 
 
-calculo = Disciplina("calculo", 1)
+def cria_disciplinas(numero):
 
+    disciplinas = []
+    i=0
+    while i < numero:
+        
+        nome = input("\nDigite o nome da disciplina:")
+        codigo = int(input("Digite o codigo da disciplina:"))
+        periodo = int(input("Digite o periodo da disciplina:"))
+        
+        disciplina = Disciplina(nome, codigo, periodo)
+        disciplinas.append(disciplina)
+        i+=1
 
-c = Curriculo("Engenharia da computacao")
-print(c.curso)
-c.adicionar_disciplina(calculo)
+    return disciplinas
 
-c.mostrar_disciplinas()
+disciplinas = cria_disciplinas(2)
+
+criaCurriculo("Engenharia da computacao", disciplinas)
